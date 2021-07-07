fc925af22471772e587425d13c6b389a26ae03b6
Charlie Liu <15334486+CLiu13@users.noreply.github.com>
2021-07-07 16:53:36 -0400

    Display elected officials for input address
    The display method in this commit is a temporary proof-of-concept.
    Note: renames civic-api.js -> main.js

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,8 +1,7 @@
 /**
  * Build and execute request to look up elected officials for provided address.
  * @param {string} address Address for which to fetch elected officials info.
- * @param {function(Object)} callback Function which takes the
- *     response object as a parameter.
+ * @param {function(Object)} callback Function which takes the response object as a parameter.
  */
 function lookup(address, callback) {
     /**
@@ -10,8 +9,8 @@ function lookup(address, callback) {
      * @type {gapi.client.HttpRequest}
      */
     var req = gapi.client.request({
-        'path' : '/civicinfo/v2/representatives',
-        'params' : {'address' : address}
+        "path" : "/civicinfo/v2/representatives",
+        "params" : {"address" : address}
     });
     req.execute(callback);
 }
@@ -22,44 +21,49 @@ function lookup(address, callback) {
  * @param {Object} rawResponse Raw response from the API.
  */
 function renderResults(response, rawResponse) {
-    var el = document.getElementById('results');
+    var el = document.getElementById("results");
     if (!response || response.error) {
         el.appendChild(document.createTextNode(
-            'Error while trying to fetch elected officials'));
+            "Error while trying to fetch elected officials"));
         return;
     }
-    var normalizedAddress = response.normalizedInput.line1 + ' ' +
-        response.normalizedInput.city + ', ' +
-        response.normalizedInput.state + ' ' +
+    var normalizedAddress = response.normalizedInput.line1 + " " +
+        response.normalizedInput.city + ", " +
+        response.normalizedInput.state + " " +
         response.normalizedInput.zip;
     if (response.officials.length > 0) {
         for (let i = 0; i < response.officials.length; i++) {
-            var normEl = document.createElement('p');
+            var normEl = document.createElement("p");
             normEl.appendChild(document.createTextNode(response.officials[i].name));
             el.appendChild(normEl);
         }
     } else {
         el.appendChild(document.createTextNode(
-            'Could not find elected officials for ' + normalizedAddress));
+            "Could not find elected officials for " + normalizedAddress));
     }
 }
 
 /**
- * Initialize the API client and make a request.
+ * Initialize the API client.
  */
-function start() {
-    gapi.client.setApiKey('INSERT API KEY HERE');
-    lookup('1263 Pacific Ave. Kansas City KS', renderResults);
+ function start() {
+    gapi.client.setApiKey("INSERT API KEY HERE");
 }
 
+/**
+ * Process form data, display address, and search for elected officials.
+ */
 function load() {
-    var inputAddress = window.location.href.split("input-address=")[1].split("&")[0].split("+").join(" ");
+    var inputStreet = window.location.href.split("input-street=")[1].split("&")[0].split("+").join(" ");
     var inputCity = window.location.href.split("input-city=")[1].split("&")[0].split("+").join(" ");
     var inputState = window.location.href.split("input-state=")[1].split("&")[0];
     var inputZip = window.location.href.split("input-zip=")[1].split("&")[0];
 
     var displayAddress = document.getElementById("display-address");
-    displayAddress.innerHTML = inputAddress + "<br>" + inputCity + ", " + inputState + " " + inputZip;
+    displayAddress.innerHTML = inputStreet + "<br>" + inputCity + ", " + inputState + " " + inputZip;
+
+    var normalizedAddress = inputStreet + ", " + inputCity + ", " + inputState + " " + inputZip;
+    lookup(normalizedAddress, renderResults);
 }
 
-// gapi.load('client', start);
+gapi.load("client", start);
