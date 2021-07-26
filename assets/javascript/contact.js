a60ea88d1c0d6358132976ec4bc5c3ffe692f79b
Jacob Cuomo <jacob.cuomo@gsa.gov>
2021-07-26 10:52:04 -0500

    Add valid email for contact page

--- assets/javascript/contact.js
+++ assets/javascript/contact.js
@@ -2,15 +2,8 @@
  * Process form data, display address, and search for elected officials.
  */
  function load() {
-    var inputStreet = window.location.href.split("input-street=")[1].split("&")[0].split("+").join(" ");
-    var inputCity = window.location.href.split("input-city=")[1].split("&")[0].split("+").join(" ");
-    var inputState = window.location.href.split("input-state=")[1].split("&")[0];
-    var inputZip = window.location.href.split("input-zip=")[1].split("&")[0];
+    var inputEmail = window.location.href.split("input-email=")[1].replace("_", "@");
 
-    var inputEmail = window.location.href.split("input-email")[1].split("&")[0];
-
-    var normalizedAddress = inputStreet + ", " + inputCity + ", " + inputState + " " + inputZip;
-
-    var displayAddress = document.getElementById("display-address");
-    displayAddress.innerHTML = normalizedAddress.replace(", ", "<br>");
+    var mailForm = document.getElementById("mailForm");
+    mailForm.setAttribute("action", "mailto:" + inputEmail);
 }
