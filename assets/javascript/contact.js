3bad07a5e06de715480be510758bd3fcd528a227
Jacob Cuomo <jacob.cuomo@gsa.gov>
2021-08-05 10:36:04 -0500

    possible email fix

--- assets/javascript/contact.js
+++ assets/javascript/contact.js
@@ -7,9 +7,25 @@
     let mailForm = document.getElementById("mailForm");
     mailForm.setAttribute("action", "mailto:" + inputEmail);
 
+
     // In case the mailto button doesn't work,
     // display email for user to manually input
     let buttonAlt = document.getElementById("button-alt");
     let message = "Having trouble with the button above? Send your concern directly to " + inputEmail;
     buttonAlt.innerHTML = message;
+}
+
+function setAction() {
+    const inputEmail = window.location.href.split("input-email=")[1].replace("_", "@");
+    let whatIssue = document.getElementById("input-type-issue");
+    let aboutIssue = document.getElementById("input-type-about");
+    let actionIssue = document.getElementById("input-type-action");
+
+    let mailForm = document.getElementById("mailForm");
+
+    let body = "The issue I am inquiring about is: %0D%0A" + whatIssue.value + "%0D%0A"
+                + "What I have to say about the issue is: %0D%0A" + aboutIssue.value + "%0D%0A";
+                + "The action I would like to be taken is: %0D%0A" + actionIssue.value + "%0D%0A";
+    let mailAction = "mailto: " + inputEmail + "?" + "subject=Hi, I was inquiring about an issue&body=" + body;
+    mailForm.setAttribute("action", mailAction);
 }
